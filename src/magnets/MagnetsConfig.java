2d479164a1d29290f346de15a6bdaa490a8eec6c
Jerry Chen <jc2939@rit.edu>
2022-11-06 18:25:00 -0500

    Implemented both constructors, getSuccessors method, and IMagnet test methods

--- src/magnets/MagnetsConfig.java
+++ src/magnets/MagnetsConfig.java
@@ -40,6 +40,18 @@ public class MagnetsConfig implements Configuration, IMagnetTest {
     private final static char BOTTOM = 'B';
     /** and ignored count for pos/neg row/col */
     private final static int IGNORED = -1;
+    private int amountOfRows;
+    private int amountOfCols;
+    private int cursorRow;
+    private int cursorCol;
+    private char[][] board;
+    private char[][] grid;
+    private int[][] cursor;
+    private int[] posRow;
+    private int[] posCol;
+    private int[] negRow;
+    private int[] negCol;
+
 
     // TODO
     // add private state here
@@ -55,11 +67,59 @@ public class MagnetsConfig implements Configuration, IMagnetTest {
      * @throws IOException thrown if there is a problem opening or reading the file
      */
     public MagnetsConfig(String filename) throws IOException {
-        try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
+        System.out.println("File: " + filename);
+        try (BufferedReader in = new BufferedReader(new FileReader(filename)))
+        {
             // read first line: rows cols
             String[] fields = in.readLine().split("\\s+");
+            this.amountOfRows = Integer.parseInt(fields[0]);
+            this.amountOfCols = Integer.parseInt(fields[1]);
+            this.board = new char[amountOfRows][amountOfCols];
+            this.grid = new char[amountOfRows][amountOfCols];
+            fields = in.readLine().split("\\s+");
+            this.posRow = new int[amountOfRows];
+            for (int i = 0; i < amountOfRows; i++)
+            {
+                this.posRow[i] = Integer.parseInt(fields[i]);
+            }
 
-            // TODO: Finish implementing the constructor
+            fields = in.readLine().split("\\s+");
+            this.posCol = new int[amountOfCols];
+            for (int i = 0; i < amountOfCols; i++)
+            {
+                this.posCol[i] = Integer.parseInt(fields[i]);
+            }
+
+            fields = in.readLine().split("\\s+");
+            this.negRow = new int[amountOfRows];
+            for (int i = 0; i < amountOfRows; i++)
+            {
+                this.negRow[i] = Integer.parseInt(fields[i]);
+            }
+
+            fields = in.readLine().split("\\s+");
+            this.negCol = new int[amountOfCols];
+            for (int i = 0; i < amountOfCols; i++)
+            {
+                this.negCol[i] = Integer.parseInt(fields[i]);
+            }
+
+
+            for (int row = 0; row < amountOfRows; row++) {
+                fields = in.readLine().split("\\s+");
+                char[] c = new char[amountOfCols];
+                for (int i = 0; i < amountOfCols; i++)
+                    c[i] = fields[i].charAt(0);
+                for (int col = 0; col < amountOfCols; col++)
+                    grid[row][col] = c[col];
+            }
+
+            for (int row = 0; row < amountOfRows; row++)
+                for (int col = 0; col < amountOfCols; col++)
+                    board[row][col] = EMPTY;
+
+            this.cursorRow = 0;
+            this.cursorCol = -1;
 
         } // <3 Jim
     }
@@ -71,7 +131,33 @@ public class MagnetsConfig implements Configuration, IMagnetTest {
      * @param val the value to store at new cursor location
      */
     private MagnetsConfig(MagnetsConfig other, char val) {
-       // TODO
+       this.amountOfRows = other.amountOfRows;
+       this.amountOfCols = other.amountOfCols;
+       this.cursorRow = other.cursorRow;
+       this.cursorCol = other.cursorCol;
+
+       this.cursorCol += 1;
+       if (this.cursorCol == amountOfCols)
+       {
+           this.cursorRow += 1;
+           this.cursorCol = 0;
+       }
+       this.board = new char[amountOfRows][amountOfCols];
+       for (int row = 0; row < amountOfRows; row++)
+           System.arraycopy(other.board[row], 0, this.board[row], 0, amountOfCols);
+
+       if (val == POS)
+       {
+           this.board[this.cursorRow][this.cursorCol] = POS;
+       }
+       if (val == NEG)
+       {
+           this.board[this.cursorRow][cursorCol] = NEG;
+       }
+       if (val == BLANK)
+       {
+           this.board[cursorRow][cursorCol] = BLANK;
+       }
     }
 
 
@@ -84,9 +170,12 @@ public class MagnetsConfig implements Configuration, IMagnetTest {
     @Override
     public List<Configuration> getSuccessors() {
         List<Configuration> successors = new ArrayList<>();
-
-        // TODO
-
+        MagnetsConfig child = new MagnetsConfig(this, '+');
+        MagnetsConfig child2 = new MagnetsConfig(this, '-');
+        MagnetsConfig child3 = new MagnetsConfig(this, 'X');
+        successors.add(child);
+        successors.add(child2);
+        successors.add(child3);
         return successors;
     }
 
@@ -174,60 +263,51 @@ public class MagnetsConfig implements Configuration, IMagnetTest {
 
     @Override
     public int getRows() {
-        // TODO
-        return 0;
+        return this.amountOfRows;
     }
 
     @Override
     public int getCols() {
-        // TODO
-        return 0;
+        return this.amountOfCols;
     }
 
     @Override
     public int getPosRowCount(int row) {
-        // TODO
-        return 0;
+        return this.posRow[row];
     }
 
     @Override
     public int getPosColCount(int col) {
-        // TODO
-        return 0;
+        return this.posCol[col];
     }
 
     @Override
     public int getNegRowCount(int row) {
-        // TODO
-        return 0;
+        return this.negRow[row];
     }
 
     @Override
     public int getNegColCount(int col) {
-        // TODO
-        return 0;
+        return this.negCol[col];
     }
 
     @Override
     public char getPair(int row, int col) {
-        // TODO
-        return 0;
+        return this.grid[row][col];
     }
 
     @Override
     public char getVal(int row, int col) {
-        // TODO
-        return 0;    }
+        return this.board[row][col];
+    }
 
     @Override
     public int getCursorRow() {
-        // TODO
-        return 0;
+        return this.cursorRow;
     }
 
     @Override
     public int getCursorCol() {
-        // TODO
-        return 0;
+        return this.cursorCol;
     }
 }
